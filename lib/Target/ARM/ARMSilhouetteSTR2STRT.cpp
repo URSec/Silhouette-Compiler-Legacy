ea1cfde703cae3a795aef64eaeda41096bf3acae
Jie Zhou <jzhou41@cs.rochester.edu>
2019-02-25 22:28:41 -0500

    Added the initial code to build an unprivileged store for a normal store
    Added the initial code to build a new store which is the unprivileged
    version of a normal store. Currently it only handles
        STR (immediate) -> STRT
    We need support all the other stores by expanding this initial method or
    by adding new method(s).

--- lib/Target/ARM/ARMSilhouetteSTR2STRT.cpp
+++ lib/Target/ARM/ARMSilhouetteSTR2STRT.cpp
@@ -36,6 +36,44 @@ const char *ARMSilhouetteSTR2STRT::getPassName() const {
 
 
 //
+// Method: buildUnprivStore()
+//
+// Description:
+//   This method builds an unprivileged store for a normal store. 
+//   Currently it only handles STR -> STRT. We need support all the other stores
+//   by expanding this method or adding new method(s).
+//
+// Inputs:
+//   MBB - The MachineBasicBlock in which to insert the new unprivileged store.
+//   MI - The MachineInstr before which to insert the the new unprivileged store.
+//   sourceReg - The register whose contents will be stored to some memory address.
+//   baseReg - The register used as the base register to compute the memory address.
+//   imm - The immediate that is added to the baseReg to compute the memory address.
+//   newInstrOpcode - The opcode of the unprivileged store.
+//   DL - A reference to the DebugLoc structure.
+//   TII - A pointer to the TargetInstrInfo structure.
+//
+// Outputs:
+//   A new unprivileged store.
+//
+// Return:
+//   None.
+//
+void buildUnprivStore(MachineBasicBlock &MBB,
+                     MachineInstr *MI,
+                     unsigned sourceReg, unsigned baseReg, int64_t imm,
+                     unsigned newInstrOpcode,
+                     DebugLoc &DL,
+                     const TargetInstrInfo *TII) {
+  // insert a new unprivileged store
+  BuildMI(MBB, MI, DL, TII->get(newInstrOpcode))
+    .addReg(sourceReg)
+    .addReg(baseReg)
+    .addImm(imm);
+}
+
+
+//
 // Method: debugHelper()
 //
 // Description:
@@ -44,7 +82,7 @@ const char *ARMSilhouetteSTR2STRT::getPassName() const {
 //   you can add 
 //     errs() << "some_instr's opcode = " << ARM::some_instr" << "\n"
 //   in this method.
-//   Putting all the printing or dumping in one method makes the code cleaner.
+//   Putting all the printing or dumping in this method makes the code cleaner.
 //
 // Inputs:
 //   MF - A reference to the MachineFunction this pass is currently processing.
@@ -81,6 +119,48 @@ void debugHelper(MachineFunction &MF) {
 //   false - The MachineFunction was not transformed.
 //
 bool ARMSilhouetteSTR2STRT::runOnMachineFunction(MachineFunction &MF) {
+  const TargetInstrInfo *TII = MF.getSubtarget<ARMSubtarget>().getInstrInfo();
+  DebugLoc DL;
+
+  // iterate over all MachineInstr
+  for (MachineBasicBlock &MBB : MF) {
+    std::vector<MachineInstr *> originalStores;  // we need delete the original stores
+    for (MachineInstr &MI : MBB) {
+      unsigned opcode = MI.getOpcode();
+      unsigned sourceReg = 0;
+      unsigned baseReg = 0;
+      int64_t imm = 0;
+
+      switch (opcode) {
+        case ARM::tSTRspi:
+        case ARM::t2STR_PRE:
+          // It's a STR instruction.
+          sourceReg = MI.getOperand(0).getReg();
+          baseReg = MI.getOperand(1).getReg();
+          imm = MI.getOperand(2).getImm();
+          // The imm of a STR(immediate) = ZeroExtend(imm: "00", 32); but the imm 
+          // of the STRT instruction is not ZeroExtended. 
+          imm <<= 2; 
+          buildUnprivStore(MBB, &MI, sourceReg, baseReg, imm, ARM::t2STRT, DL, TII);
+
+          originalStores.push_back(&MI);
+          break;
+        
+        default:
+          if (MI.mayStore()) {
+              errs() << "Silhouette: other stores; dump: \n";
+              MI.dump();
+          }
+          break;
+      }
+
+    }
+
+    // remove the original stores
+    for (MachineInstr *MI : originalStores) {
+      MI->eraseFromParent();
+    }
+  }
 
   // This pass modifies the program.
   return true;
