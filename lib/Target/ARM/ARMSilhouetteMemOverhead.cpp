50d931199ae441eb609d6422e51abffd10a0586b
Jie Zhou <jzhou41@cs.rochester.edu>
2019-03-02 20:42:16 -0500

    Implemented a simple memory overhead computation pass
    Currently the memory overhead computation is an estimate: the mayLoad()
    and mayStore() may return true for some non-load/store instructions.
    For an accurate implementation, we need examine every instruction
    individually.
    
    We will do that once we're sure that the the shadow stack strategy works
    and the memory overhead is acceptable.

--- lib/Target/ARM/ARMSilhouetteMemOverhead.cpp
+++ lib/Target/ARM/ARMSilhouetteMemOverhead.cpp
@@ -10,19 +10,30 @@
 // This pass estimates the memory overhead of converting all normal loads and 
 // store instructions to unprivileged loads and stores. It is an estimation 
 // because we use mayLoad() and mayStore() to check if an instruction is a load 
-// or a store; however, these two functions are not 100% accurate.
+// or a store; however, these two functions are not 100% accurate as it'd report 
+// some non-load/store instructions.
 //
 //===----------------------------------------------------------------------===//
 //
 
 #include "ARMSilhouetteMemOverhead.h"
 #include "ARMTargetMachine.h"
+#include "llvm/ADT/Statistic.h"
 #include "llvm/CodeGen/MachineFunction.h"
 #include "llvm/CodeGen/MachineBasicBlock.h"
 #include "llvm/CodeGen/MachineInstr.h"
 #include "llvm/Support/raw_ostream.h"
 
+#define BYTE_TO_MB 1024
+#define DEBUG_TYPE "arm-silhouette-mem-overhead"
+
 using namespace llvm;
+using namespace std;
+
+// memory overhead stats
+STATISTIC(MEM_OVERHEAD, "Memory overhead in Bytes");
+STATISTIC(MEM_OP_SIZE, "Memory operation instruction size");
+STATISTIC(CODE_SIZE, "Original code size in Bytes");
 
 char ARMSilhouetteMemOverhead::ID = 0;
 
@@ -54,7 +65,38 @@ StringRef ARMSilhouetteMemOverhead::getPassName() const {
 //   false - The MachineFunction was not transformed.
 //
 bool ARMSilhouetteMemOverhead::runOnMachineFunction(MachineFunction &MF) {
-  
+  // This variable is used to call method getInstSizeInBytes().
+  const ARMBaseInstrInfo *ABII = MF.getSubtarget<ARMSubtarget>().getInstrInfo();
+
+  unsigned long totalInstrSize = 0;
+  unsigned long memOpSize = 0;  // all load and store instruction size
+  unsigned int memIncreased = 0;
+
+  for (MachineBasicBlock &MBB : MF) {
+    for (MachineInstr &MI : MBB) {
+      int instrSize = ABII->getInstSizeInBytes(MI);
+      totalInstrSize += instrSize;
+      CODE_SIZE += instrSize;
+
+      if (MI.mayStore() || MI.mayLoad()) {
+        memOpSize += 2;
+        MEM_OP_SIZE += 2;
+        if (instrSize == 2) {
+          // All the unprivileged loads and store instructions are 4 bytes long
+          // in the thumb ISA.
+          memIncreased += 2;         
+          MEM_OVERHEAD += 2;
+        }
+      }
+    }
+  }
+
+  errs() << "Function " << MF.getName() << ":\n";
+  errs() << "Total code size  = " << totalInstrSize << " bytes.\n";
+  errs() << "Increase code size = " << memIncreased << " bytes (" << 
+    format("%.2f", (float(memIncreased)) / totalInstrSize * 100) << "%).\n";
+
+  // This pass doesn't change the code.
   return false;
 }
 
