6bf3390265d47f15dcdf401c85faf61e35b90b07
LeveyLeonhardt <edwarddu1996@gmail.com>
2019-05-13 18:24:46 -0400

    Added blacklist

--- lib/Target/ARM/ARMSilhouetteConvertFuncList.h
+++ lib/Target/ARM/ARMSilhouetteConvertFuncList.h
@@ -61,9 +61,9 @@ const static std::set<std::string> funcWhitelist = {
 
   // BEEBS benchmark
 #if 1
-  "main",
+  // "main",
   "initialise_benchmark",
-  "benchmark",
+  // "benchmark",
   "verify_benchmark",
   // aha-compress
   "compress3",
@@ -82,6 +82,13 @@ const static std::set<std::string> funcWhitelist = {
   "dequeue",
   "qcount",
   "dijkstra",
+  // ndes
+  "des",
+  "ks",
+  "cyfun",
+  "foo",
+  "foo2",
+  "bar",
 #endif
 };
 
@@ -118,10 +125,51 @@ const static std::set<std::string> funcBlacklist {
     // "HAL_GPIO_Init",
     // "BSP_COM_Init",
     // "HAL_UART_Transmit",
-    //     "HAL_UART_Init",
+    //     
     "HAL_TIM_IRQHandler",            // Timer interrupt handler
     "SysTick_Handler",               // Timer related handler
     "UART_CheckIdleState",           // Timer related function
     "UART_AdvFeatureConfig",         // Timer related function
     "UART_SetConfig",                // Timer related function
+    "TIM6_DAC_IRQHandler",           // Calls HAL_TIM_IRQHandler
+    "BSP_COM_Init",                  // Calls HAL_UART_Init
+    "HAL_UART_Init",                 // Calls UART_SetConfig
+    "HAL_RCC_GetSysClockFreq",       // Called by UART_SetConfig
+    "HAL_RCC_GetPCLK1Freq",          // Called by UART_SetConfig
+    "HAL_RCC_GetPCLK2Freq",          // Called by UART_SetConfig
+    "HAL_SYSTICK_IRQHandler",
+    "HAL_SYSTICK_Callback",
+    // Other HAL control functions
+    "HAL_IncTick",
+    "HAL_GetTick",
+    "HAL_Delay",
+    "HAL_SuspendTick",
+    "HAL_ResumeTick",
+    "HAL_GetHalVersion",
+    "HAL_GetREVID",
+    "HAL_GetDEVID",
+    // Other HAL init/de-init functions
+    "HAL_DeInit",
+    "HAL_MspDeInit",
+    // Other HAL Time Base functions
+    "HAL_TIM_Base_DeInit",
+    "HAL_TIM_Base_MspDeInit",
+    "HAL_TIM_Base_MspDeInit",
+    "HAL_TIM_Base_Start",
+    "HAL_TIM_Base_Stop",
+    // "HAL_TIM+Base_Start_IT", // Added already above
+    "HAL_TIM_Base_Stop_IT",
+    "HAL_TIM_Base_Start_DMA",
+    "HAL_TIM_Base_Stop_DMA",
+    // HAL Time Callback functions
+    "HAL_TIM_PeriodElapsedCallback",
+    "HAL_TIM_OC_DelayElapsedCallback",
+    "HAL_TIM_IC_CaptureCallback",
+    "HAL_TIM_PWM_PulseFinishedCallback",
+    "HAL_TIM_TriggerCallback",
+    "HAL_TIM_ErrorCallback",
+    // HAL TIMEx Callbacks
+    "HAL_TIMEx_CommutationCallback",
+    "HAL_TIMEx_BreakCallback",
+    "HAL_TIMEx_Break2Callback",
 };
