URSec/Silhouette-Compiler-Legacy

commit e65aa8611c997ab481b9202bb5be35117ce3cd55

Eliminated an unnecessary register spill in the CFI pass.
Eliminated an unnecessary register spill in the CFI pass by using IP
(R12) as the scratch register when possible.

lib/Target/ARM/ARMSilhouetteLabelCFI.cpp
@@ -153,10 +153,15 @@ ARMSilhouetteLabelCFI::insertCFICheck(MachineInstr & MI, unsigned Reg) {
   MachineBasicBlock & MBB = *MI.getParent();
   const TargetInstrInfo * TII = MBB.getParent()->getSubtarget().getInstrInfo();
 
-  unsigned ScratchReg = Reg == ARM::R5 ? ARM::R4 : ARM::R5;
-
-  // Backup a scratch register so that it is free to use
-  BackupRegister(MI, ScratchReg);
+  //
+  // Use IP (R12) as a scratch register if possible; otherwise spill and
+  // restore R4.
+  //
+  unsigned ScratchReg = ARM::R12;
+  if (ScratchReg == Reg) {
+    ScratchReg = ARM::R4;
+    BackupRegister(MI, ScratchReg);
+  }
 
   //
   // Build the following instruction sequence:
@@ -204,8 +209,10 @@ ARMSilhouetteLabelCFI::insertCFICheck(MachineInstr & MI, unsigned Reg) {
     .add(condCodeOp());
   }
 
-  // Restore the scratch register
-  RestoreRegister(MI, ScratchReg);
+  // Restore the scratch register if we spilled it
+  if (ScratchReg != ARM::R12) {
+    RestoreRegister(MI, ScratchReg);
+  }
 }
 
 //

commit 310df7023781f43e380f288b225c1e745c2e097b

Fixed a bug introduced in e65aa86.
Fixed a bug introduced in e65aa86 where we universally use R12 as a
scratch register for CFI instrumentation.  This is incorrect because:
* We are not only instrumenting indirect calls but also indirect
  branches.
* R12 might be live before an indirect branch.

This is fixed by the utility function introduced in a49bfc3.

lib/Target/ARM/ARMSilhouetteLabelCFI.cpp
@@ -161,11 +161,15 @@ ARMSilhouetteLabelCFI::insertCFICheck(MachineInstr & MI, unsigned Reg,
   const TargetInstrInfo * TII = MBB.getParent()->getSubtarget().getInstrInfo();
 
   //
-  // Use IP (R12) as a scratch register if possible; otherwise spill and
+  // Try to find a free register first.  If we are unlucky, spill and (later)
   // restore R4.
   //
-  unsigned ScratchReg = ARM::R12;
-  if (ScratchReg == Reg) {
+  unsigned ScratchReg;
+  std::deque<unsigned> FreeRegs = findFreeRegisters(MI);
+  if (!FreeRegs.empty()) {
+    ScratchReg = FreeRegs[0];
+  } else {
+    errs() << "[CFI] Unable to find a free register for " << MI;
     ScratchReg = ARM::R4;
     BackupRegister(MI, ScratchReg);
   }
@@ -218,7 +222,7 @@ ARMSilhouetteLabelCFI::insertCFICheck(MachineInstr & MI, unsigned Reg,
   }
 
   // Restore the scratch register if we spilled it
-  if (ScratchReg != ARM::R12) {
+  if (FreeRegs.empty()) {
     RestoreRegister(MI, ScratchReg);
   }
 }